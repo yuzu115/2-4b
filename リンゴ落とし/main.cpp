f40f34014ab3ecbbb52636eb83598a15b7c39f4c
Capricorn4879 <120348291+Capricorn4879@users.noreply.github.com>
2023-04-10 14:22:39 +0900

    main、infomation作成

---
+++ リンゴ落とし/main.cpp
@@ -0,0 +1,25 @@
+#include"DxLib.h"
+#include"infomation.h"
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	SetMainWindowText("リンゴ落とし");
+	ChangeWindowMode(TRUE);
+
+	// ウィンドウサイズを設定
+	SetGraphMode(SCREENSIZE_X,SCREENSIZE_Y,32);
+
+	if (DxLib_Init() == -1) return -1;     //DXライブラリの初期化処理
+	SetDrawScreen(DX_SCREEN_BACK);         //描画先画面を裏にする
+
+
+
+	while (ProcessMessage() == 0 && GameMode != CLOSE && !(g_KeyFlg & PAD_INPUT_START))
+	{
+	
+
+	}
+
+
+}
