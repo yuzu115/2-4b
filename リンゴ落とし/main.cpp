ed974a8cd793c8216f5e764e7931b134fd501e20
AtsuneSeragaki <120348226+AtsuneSeragaki@users.noreply.github.com>
2023-04-10 14:49:19 +0900

    main追加

--- リンゴ落とし/main.cpp
+++ リンゴ落とし/main.cpp
@@ -17,6 +17,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	while (ProcessMessage() == 0 && GameMode != CLOSE && !(g_KeyFlg & PAD_INPUT_START))
 	{
+		//キー入力取得 
+		g_OldKey = g_NowKey;
+		g_NowKey = GetJoypadInputState(DX_INPUT_KEY_PAD1);    //例のコントローラーの入力も使えます
+		g_KeyFlg = g_NowKey & ~g_OldKey;
+
+		ClearDrawScreen();                 //画面を初期化
 
 		DrawBox(0, 0, 1280, 720, 0xd3d3d3, TRUE);
 
@@ -24,6 +30,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	}
 
+	DxLib_End();				           // ＤＸライブラリ使用の終了処理
 
+	return 0;				               // ソフトの終了 
 }
 
