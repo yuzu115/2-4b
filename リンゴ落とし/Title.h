68eafacc0c044d0e0766dababa0f763aec70a56c
chihiro-hiro <120348424+chihiro-hiro@users.noreply.github.com>
2023-04-12 11:08:21 +0900

    画像の追加、Title.hへの追加
    タイトルの変数、関数の追加

--- リンゴ落とし/Title.h
+++ リンゴ落とし/Title.h
@@ -3,3 +3,11 @@
 //タイトル画面のみで使う変数・関数の宣言をここに書きます
 
 //画像変数もここに書いてください
+
+int gTitleImg;				// タイトル画像
+int gTitleFontImg;			// タイトルフォント画像
+
+static int menuNo = 0;		// START、HELP、ENDいずれかの状態
+int posY;					// カーソル移動
+
+void DrawTitle(void);
