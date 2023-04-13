1f8d6551691fb2c83d289250676d9e9621135bab
chihiro-hiro <120348424+chihiro-hiro@users.noreply.github.com>
2023-04-13 10:17:36 +0900

    タイトルでキー操作

--- リンゴ落とし/Title.cpp
+++ リンゴ落とし/Title.cpp
@@ -1,6 +1,13 @@
-#include <stdio.h>
 #include "Title.h"
 
-void DrawTitle() {
+void DrawTitle(void) {
+
+	// タイトル画像の読込
+	if ((TitleImg = LoadGraph("images/Title.bmp")) == -1) return -1;
+
+	// タイトル画像、メニューカーソル（三角形）の表示
+	DrawGraph(0, 0, TitleImg, FALSE);
+	posY = menuNo * 45;
+	DrawTriangle(140, 210 + posY, 170, 230 + posY, 140, 250 + posY, 0xff0000, TRUE);
 
 }
--- リンゴ落とし/main.cpp
+++ リンゴ落とし/main.cpp
@@ -4,7 +4,6 @@
 #include "HELP.h"
 #include "END.h"
 
-
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -17,13 +16,23 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	if (DxLib_Init() == -1) return -1;     //DXライブラリの初期化処理
 	SetDrawScreen(DX_SCREEN_BACK);         //描画先画面を裏にする
 
-
-
 	while (ProcessMessage() == 0 && GameMode != CLOSE && !(g_KeyFlg & PAD_INPUT_START))
 	{
-	
+		//入力キー取得
+		g_OldKey = g_NowKey;
+		g_NowKey = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+		g_KeyFlg = g_NowKey & ~g_OldKey;
+
+		switch (GameMode) {
+			case TITLE:
+				DrawTitle();		//ゲームタイトル描画処理
+				break;
+		}
+
+		ScreenFlip();	//裏画面の内容を表画面に反映
 
 	}
+	DxLib_End();	//DXライブラリ使用の終了処理
 
-
+	return 0;	//ソフトの終了
 }
