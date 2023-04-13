14575cc1b161727c27d3c205cd49a57abb621071
yuzu115 <120348154+yuzu115@users.noreply.github.com>
2023-04-13 22:55:35 +0900

    ポーズ画面遷移

--- リンゴ落とし/Pause.cpp
+++ リンゴ落とし/Pause.cpp
@@ -1,2 +1,20 @@
 #include "DxLib.h"
 #include "Pause.h"
+
+int gStartKeyFlg = FALSE;
+
+/********************
+*ポーズ画面遷移     *
+********************/
+void pause(void)
+{
+	if (keyflg & PAD_INPUT_8) 
+	{
+		gStartKeyFlg = TRUE;
+	}
+	if (gStartKeyFlg = TRUE) 
+	{
+		SetFontSize(100);
+		DrawString(350, 300, "--ポーズ画面--", 0xffffff);
+	}
+}
