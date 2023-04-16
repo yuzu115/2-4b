5dbf591abfa597d1b9fc97fe50b79295a5684662
chihiro-hiro <120348424+chihiro-hiro@users.noreply.github.com>
2023-04-16 23:27:10 +0900

    タイトル画像表示

--- リンゴ落とし/Title.cpp
+++ リンゴ落とし/Title.cpp
@@ -1,6 +1,15 @@
 #include "Title.h"
 
 
-void DrawTitle(void) {
-	DrawFormatString(400, 220, 0xffffff, "タイトル");
+int DrawTitle(void) {
+
+	// タイトル画像の読込
+	if ((titleImg = LoadGraph("images/Title.png")) == -1) return -1;
+
+	// タイトル画像、メニューカーソル（三角形）の表示
+	DrawGraph(0, 0, titleImg, FALSE);
+	posY = menuNo * 45;
+	DrawTriangle(140, 210 + posY, 170, 230 + posY, 140, 250 + posY, 0xff0000, TRUE);
+
+	//DrawFormatString(400, 220, 0xffffff, "タイトル");
 }
--- リンゴ落とし/Title.h
+++ リンゴ落とし/Title.h
@@ -3,5 +3,9 @@
 //タイトル画面のみで使う変数・関数の宣言をここに書きます
 
 //画像変数もここに書いてください
+int titleImg;
 
-void DrawTitle();
+static int menuNo;
+int posY;
+
+int DrawTitle(void);
