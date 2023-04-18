457ad86d5928dcf9f2a2091b8830f5eb8aa27c4c
yuzu115 <120348154+yuzu115@users.noreply.github.com>
2023-04-18 11:11:36 +0900

    保存

--- リンゴ落とし/Time.cpp
+++ リンゴ落とし/Time.cpp
@@ -1,6 +1,14 @@
 #include "DxLib.h"
 #include "Time.h"
 
+//•Ï”éŒ¾
+int NowTime;
+int NowCnt;
 
+//ƒ^ƒCƒ€‚Ìİ’è
+void Timer(void)
+{
+
+}
 
 
--- リンゴ落とし/Time.h
+++ リンゴ落とし/Time.h
@@ -2,4 +2,6 @@
 #ifndef TIME_H_
 #define TIME_H_
 
+void Timer(void);//ƒ^ƒCƒ€‚Ìİ’è
+
 #endif // !TIME_H_
