2c6e92b9681e5795d8726dac2221782ae28f8365
Capricorn4879 <120348291+Capricorn4879@users.noreply.github.com>
2023-04-19 13:31:07 +0900

    コメント

--- リンゴ落とし/DrawApple.cpp
+++ リンゴ落とし/DrawApple.cpp
@@ -5,7 +5,7 @@
 * 定数の宣言
 ******************************************/
 const int APPLE_MAX = 10;	//リンゴの最大生成数
-const int APPLE_TYPE=4;
+const int APPLE_TYPE=4;		//リンゴの種類
 
 /******************************************
 * 構造体の宣言
@@ -20,19 +20,25 @@ struct AppleDate {
 	int color;		//色(仮)
 };
 
+//リンゴの変数
 struct AppleDate gApple[APPLE_MAX];
 
-struct AppleDate gApple00 = { TRUE,0,0,0,-40,45,1,0 };
-
-// これをいれたいよ
+//各リンゴのデータ
 struct AppleDate gApple_Rd = {TRUE,0,0,0,-40,45,1,100,0xff0000};	//赤
 struct AppleDate gApple_Br = {TRUE,1,0,0,-40,45,2,200,0x00ff00}; //青
 struct AppleDate gApple_Gl = {TRUE,2,0,0,-40,45,3.5,500,0xffff00}; //金
 struct AppleDate gApple_Po = {TRUE,3,0,0,-40,45,0.5,-750,0xff00ff}; //毒
 
+//ステータス格納変数
 struct AppleDate gAppleState[APPLE_TYPE];
+
+//乱数格納変数
 int gRandApple;
 
+
+/**
+* リンゴのデータ格納
+*/
 int AppleSet(void)
 {
 	gAppleState[0] = gApple_Rd;
@@ -43,9 +49,9 @@ int AppleSet(void)
 	return 0;
 }
 	
-
-
-
+/**
+* リンゴの描画
+*/
 void DrawApple(void){
 
 
@@ -56,6 +62,7 @@ void DrawApple(void){
 			DrawCircle(gApple[i].x, gApple[i].y, gApple[i].r, gApple[i].img, TRUE);
 			gApple[i].y += gApple[i].speed;
 
+			//gAppleのy座標が1000以下になったとき消去
 			if (gApple[i].y > 1000) {
 				gApple[i].flg = FALSE;
 			}
@@ -68,20 +75,24 @@ void DrawApple(void){
 		}
 	}
 
+	//生成関数の読み込み
 	CreateApple();
 
 }
 
 
-// リンゴの生成
+/**
+* リンゴの生成
+**/
 int CreateApple()
 {
+	//ステータスの読み込み
 	AppleSet();
 
 	for (int i = 0; i < APPLE_MAX; i++) {
 		if (gApple[i].flg == FALSE) {
-			gApple[i] = gAppleState[gApple[i].type];
-			gApple[i].type = RandApple();
+			gApple[i] = gAppleState[gApple[i].type];	//ステータスの格納
+			gApple[i].type = RandApple();				//
 			gApple[i].img = gApple[i].color;
 			gApple[i].x = GetRand(7) * 125 + 50;
 			gApple[i].speed = 1+1*gApple[i].speed;
@@ -92,25 +103,31 @@ int CreateApple()
 	return FALSE;
 }  
 
-
+/**
+* リンゴの出現率
+*/
 int RandApple()
 {
-	gRandApple = GetRand(100);
+	gRandApple = GetRand(100);	//0～100までの乱数を取得
 
 	for (int i = 0; i < APPLE_MAX;i++)
 	{
+		//0～59以下の数値なら赤リンゴ(戻り値:0)
 		if (gRandApple <= 59)
 		{
 			return 0;
 		}
+		//60～84以下の数値なら青リンゴ(戻り値:1)
 		else if (gRandApple <=84 && gRandApple>=60)
 		{
 			return 1;
 		}
+		//85～94以下の数値なら金リンゴ(戻り値:2)
 		else if (gRandApple <= 94 && gRandApple>=84)
 		{
 			return 2;
 		}
+		//95～100以下の数値なら毒リンゴ(戻り値:3)
 		else if (gRandApple <= 100 && gRandApple>=94)
 		{
 			return 3;
