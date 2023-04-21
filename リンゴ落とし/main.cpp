095869325f6e55e299e1d611f782d0a8231ad88f
git961 <120348157+git961@users.noreply.github.com>
2023-04-21 11:54:14 +0900

    FPS処理

--- リンゴ落とし/main.cpp
+++ リンゴ落とし/main.cpp
@@ -1,5 +1,8 @@
 #include"DxLib.h"
+#include"math.h"
 #include"infomation.h"
+#include"FPS.h"
+
 
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
@@ -14,12 +17,34 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	SetDrawScreen(DX_SCREEN_BACK);         //描画先画面を裏にする
 
 
+	//ScreenFlipを実行しても垂直同期信号を待たない
+		//SetWaitVSyncFlag(FALSE);
+
+	//ループ前にFPS計測を初期化
+	Reset_fps();
 
 	while (ProcessMessage() == 0 && GameMode != CLOSE && !(g_KeyFlg & PAD_INPUT_START))
 	{
-	
+
+		//画面の初期化
+		ClearDrawScreen();
+		
+		//今出てるFPSの表示
+		display_fps();
+
+		//裏画面の内容を表画面に反映する
+		ScreenFlip();
+
+		//fpsの計測
+		Keisoku_fps();
+
+		ScreenFlip();
+
+		//fps固定処理
+		wait_fanc();
 
 	}
-
-
+	DxLib_End();
+	return 0;
 }
+
