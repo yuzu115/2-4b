c2971c99022f9e0290b284c1d151aa5c1b6ad368
chihiro-hiro <120348424+chihiro-hiro@users.noreply.github.com>
2023-05-19 11:10:00 +0900

    RANKINGクラス作成

--- リンゴ落とし/Ranking.h
+++ リンゴ落とし/Ranking.h
@@ -39,4 +39,31 @@ int LoadRankingImages(void);						// ランキング画像読込
 //extern RankingData Ranking[RANK_MAX];			// RankingとResultで使用
 //RankingData Ranking[RANK_MAX];			// RankingとResultで使用
 
+class RANKING
+{
+private:
+
+	typedef struct
+	{
+		int no;						// 順位
+		char name[NAME_MAX];		// 名前　10Byte
+		long score;					// スコア
+	} RankingData;
+
+	int RankingImg;			// ランキング画像
+
+public:
+	RANKING();										// コンストラクタ
+	~RANKING();										// デストラクタ
+	void DrawRanking(XINPUT_STATE input, RankingData Ranking[], int& Button_flg, int& GameMode);			// ランキング画面描画処理
+	int ReadRanking(RankingData Ranking[]);				// ランキングデータの読込
+
+	void SortRanking(RankingData Ranking[]);			// ランキング並べ替え
+	int SaveRanking(RankingData Ranking[]);				// ランキングデータの保存
+
+	//int LoadRankingImages(void);						// ランキング画像読込
+
+
+};
+
 #endif // !RANKING_H__
