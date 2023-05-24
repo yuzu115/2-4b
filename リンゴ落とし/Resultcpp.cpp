03914910afc3e614b00aecad8b7700432218a826
chihiro-hiro <120348424+chihiro-hiro@users.noreply.github.com>
2023-05-24 16:06:48 +0900

    コメントの追加

--- リンゴ落とし/Resultcpp.cpp
+++ リンゴ落とし/Resultcpp.cpp
@@ -9,17 +9,17 @@ int ResultBackImg;					// 背景画像
 
 int ResultWaitTime = 0;					// 待ち時間
 
-int RS;
-int BS;
-int GS;
-int PS;
+int RC;			// 赤リンゴの獲得数
+int BC;			// 青リンゴの獲得数
+int GC;			// 金リンゴの獲得数
+int PC;			// 毒リンゴの獲得数
 
 void GetAppScore(Apple::AppScore* as)
 {
-	RS = as->r;
-	BS = as->b;
-	GS = as->g;
-	PS = as->p;
+	RC = as->r;
+	BC = as->b;
+	GC = as->g;
+	PC = as->p;
 }
 
 /****************************************
@@ -30,8 +30,6 @@ void DrawResult(RankingData Ranking[], int& GameMode)
 	Apple app;
 
 	LoadResultImages();					// リザルト画像読込
-
-	// ReadRanking(Ranking);			//ランキングデータの読込
 
 	//タイムの加算処理(180f以上経過後)＆画面遷移
 	if (++ResultWaitTime > 180) {
@@ -45,15 +43,14 @@ void DrawResult(RankingData Ranking[], int& GameMode)
 
 	}
 
-		
 	// リザルト画像の表示
 	DrawGraph(0, 0, ResultBackImg, FALSE);
 
 	SetFontSize(50);
-	DrawFormatString(410, 180, 0x000000, "赤リンゴ : %3d コ\n",RS);
-	DrawFormatString(410, 240, 0x000000, "青リンゴ : %3d コ\n",BS);
-	DrawFormatString(410, 300, 0x000000, "金リンゴ : %3d コ\n",GS);
-	DrawFormatString(410, 360, 0x000000, "毒リンゴ : %3d コ\n",PS);
+	DrawFormatString(410, 180, 0x000000, "赤リンゴ : %3d コ\n",RC);
+	DrawFormatString(410, 240, 0x000000, "青リンゴ : %3d コ\n",BC);
+	DrawFormatString(410, 300, 0x000000, "金リンゴ : %3d コ\n",GC);
+	DrawFormatString(410, 360, 0x000000, "毒リンゴ : %3d コ\n",PC);
 	DrawFormatString(410, 500, 0x000000, "スコア   : %5d\n",app.GetScore());
 
 }
