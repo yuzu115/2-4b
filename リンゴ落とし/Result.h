f2b38c8f2acc71afa53257d3c9530164b0a97785
chihiro-hiro <120348424+chihiro-hiro@users.noreply.github.com>
2023-05-25 14:24:13 +0900

    リザルトクラス作成

--- リンゴ落とし/Result.h
+++ リンゴ落とし/Result.h
@@ -16,4 +16,17 @@ void DrawResult(RankingData Ranking[],int& GameMode);		// リザルト画面描画
 void GetAppScore(Apple::AppScore* as);
 int LoadResultImages(void);			// リザルト画像読込
 
+//class Result
+//{
+//private:
+//	int ResultBackImg;					// 背景画像
+//	int ResultWaitTime;					// 待ち時間
+//
+//public:
+//	Result();													// コンストラクタ
+//	~Result();													// デストラクタ
+//	void DrawResult(RankingData Ranking[], int& GameMode);		// リザルト画面描画
+//	int LoadResultImages(void);									// リザルト画像読込
+//};
+
 #endif // !RESULT_H__
